64228f3fee0566fe62a53ad9d6174058da3d8cf2
Rusty Myers <rustymyers@gmail.com>
2013-04-11 09:23:18 -0400

    Initial commit
    psuFusionSetup.sh initial script. Looks for two internal drives, one
    SSD and one HDD, and turns them into a CoreStorage Fusion Drive.
    Caution, you can loose data if not careful, use at your own risk!

---
+++ psuFusionSetup.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# Written by Rusty Myers
+# 2013-04-08
+
+# Script to create fusion drives
+# Use the following command to delete the Fusion Drive Setup:
+# diskutil cs delete $CoreStorageUUID
+#--------------------------------------------------------------------------------------------------
+#-- Log - Echo messages with date and timestamp
+#--------------------------------------------------------------------------------------------------
+Log ()
+{
+	logText=$1
+	# indent lines except for program entry and exit
+	if [[ "${logText}" == "-->"* ]];then
+		logText="${logText}`basename $0`: launched..."
+	else
+		if [[ "${logText}" == "<--"* ]];then
+			logText="${logText}`basename $0`: ...terminated" 
+		else
+		logText="   ${logText}"
+		fi
+	fi
+	date=$(/bin/date)
+	echo "${date/E[DS]T /} ${logText}"
+}
+
+Log "-->"
+
+
+# Reset Variables
+SSD=""
+HDD=""
+UUID=""
+DiskListArrayNumber=0
+
+# Set new name of Fusion Drive volume
+VOLUMENAME="Macintosh HD"
+GROUPNAME="CLCFusion"
+
+# Build array of internal disks in Mac (disk0, disk1, disk2, etc...)
+DiskList=`diskutil list | grep "/dev/"`
+
+for i in $DiskList; do
+	# Run through each disk connected
+	
+	# Yes if internal
+	if [[ `diskutil info $i |awk '/^   Internal:/ {print $2}'` = "Yes" ]]; then
+		Log "Disk $i is Internal"
+		# echo "Disk array number: $DiskListArrayNumber"
+		# Set array with internal disk
+		DiskListArray[$DiskListArrayNumber]="$i"
+		# Increment array
+        DiskListArrayNumber=$(expr $DiskListArrayNumber + 1)
+	# Disabling else statement because we don't need to see how many drives are not internal
+	# else
+		# Not internal, ignore
+		# echo "Disk $i is NOT Internal"
+	fi
+
+done
+
+Log "There are ${#DiskListArray[@]} internal disks in the DiskListArray"
+
+# Get disk IDs for ssd and platter, set into variables for SSD and HDD
+for i in "${DiskListArray[@]}"; do
+	Log "Checking $i for disk type (SSD/HDD)"
+	if [[ `diskutil info $i |awk '/^   Solid State:/ {print $3}'` = "Yes" ]]; then
+		SSD="$i"
+	else
+		HDD="$i"
+	fi
+done
+
+Log "Internal Drives are:"
+Log "	SSD: $SSD"
+Log "	HDD: $HDD"
+
+# Create core storage group
+diskutil cs create $GROUPNAME $SSD $HDD
+
+# Set UUID from new CoreStorage Group
+CoreStorageUUID=`diskutil cs list | awk '/Logical Volume Group/ {print $5}'`
+Log "New CoreStorage Group UUID: $CoreStorageUUID"
+
+# Sleep so core storage drives can settle for a second
+sleep 5
+
+# Create new volume from CS group UUID
+Log "Running: diskutil cs createVolume $CoreStorageUUID jhfs+ \"$VOLUMENAME\" 100%"
+diskutil cs createVolume $CoreStorageUUID jhfs+ "$VOLUMENAME" 100%
+
+Log "Fusion Drive: `diskutil cs list | grep -A10 "        +-> Logical Volume "| awk '/Volume Name:/ {print $3}'`"
+Log "New Fusion Drive setup complete."
+
+
+Log "<--"
